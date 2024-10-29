8fe83d7446043adb6127dfc496b7b2fee7f7cf29
Wes Murray <114347469+wescmx@users.noreply.github.com>
2024-10-29 14:36:50 +0200

    [1.x] Updated resource names and `alarm_description`.

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_metric_filter" "iam_metric_filter" {
+resource "aws_cloudwatch_log_metric_filter" "metric_filter" {
   log_group_name = var.cw_log_group_name
   name           = "iam-metric-filter"
   pattern        = "{ ($.eventSource = iam.amazonaws.com) && ($.eventName = \"${join("\" || $.eventName = \"", var.iam_event_names)}\") }"
@@ -10,7 +10,7 @@ resource "aws_cloudwatch_log_metric_filter" "iam_metric_filter" {
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "elb_metric_filter_alarm" {
+resource "aws_cloudwatch_metric_alarm" "metric_filter_alarm" {
   alarm_name          = "iam-metric-filter-alarm"
   comparison_operator = var.cw_metric_filter_alarm_comparison_operator
   evaluation_periods  = var.cw_metric_filter_alarm_evaluation_periods
@@ -19,6 +19,6 @@ resource "aws_cloudwatch_metric_alarm" "elb_metric_filter_alarm" {
   period              = var.cw_metric_filter_alarm_period
   statistic           = var.cw_metric_filter_alarm_statistic
   threshold           = var.cw_metric_filter_alarm_threshold
-  alarm_description   = "Alarm when IAM exceeds the specified threshold."
+  alarm_description   = "Alarm when the resource exceeds the specified threshold."
   alarm_actions       = var.cw_metric_filter_alarm_actions
 }
